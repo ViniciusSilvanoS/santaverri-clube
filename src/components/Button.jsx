796afe1232d62c32c51179382f47cb18e611b9ec
ViniciusSilvanoS <viniciussilvano022@gmail.com>
2024-07-04 14:05:39 -0300

    ajuste nos botoes e logo patrocinadores

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ children, bg = true, className }) => {
     const defaultStyle = "px-7 py-5 rounded-lg font-medium text-lg lg:"
     const styles = {
         primary: "bg-first-blue text-white hover:bg-blue-700",
-        secondary: "border border-first-blue text-first-blue hover:bg-zinc-100 hover:font-bold",
+        secondary: "border border-first-blue text-first-blue hover:bg-zinc-100",
       };
       const buttonStyle = bg ? styles.primary : styles.secondary;
   return (
